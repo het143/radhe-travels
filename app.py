950358c07911c578377874864b7580240fa17267
het143 <solankihet272@gmail.com>
2026-09-02 16:43:11 +0530

    Add MYSQL_PORT to Flask config

--- app.py
+++ app.py
@@ -69,8 +69,8 @@ app.config['MYSQL_HOST'] = config.MYSQL_HOST
 app.config['MYSQL_USER'] = config.MYSQL_USER
 app.config['MYSQL_PASSWORD'] = config.MYSQL_PASSWORD
 app.config['MYSQL_DB'] = config.MYSQL_DB
+app.config['MYSQL_PORT'] = config.MYSQL_PORT  # <-- ADD THIS EXACT LINE
 mysql.init_app(app)
-
 # ---------------------------------------------------------
 # GOOGLE OAUTH CONFIGURATION
 # ---------------------------------------------------------
